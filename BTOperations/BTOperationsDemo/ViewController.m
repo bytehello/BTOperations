e188b34d4ca6ae71ce7e42972783976422f093e4
BYTE <gechanghang@foxmail.com>
2017-11-30 18:10:35 +0800

    *

--- BTOperations/BTOperationsDemo/ViewController.m
+++ BTOperations/BTOperationsDemo/ViewController.m
@@ -8,7 +8,7 @@
 #import "ViewController.h"
 #import "BTGroupOperation.h"
 @interface ViewController ()
-
+@property (nonatomic, assign, readwrite) bool isLogin;
 @end
 
 @implementation ViewController
@@ -16,6 +16,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    /*
     NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
         NSLog(@"op1");
     }];
@@ -31,6 +32,28 @@
     [op3 addDependency:op2];
     [[NSOperationQueue currentQueue] addOperation:gop];
     [[NSOperationQueue currentQueue] addOperation:op3];
+     */
+    NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"op1");
+    }];
+    
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"op2");
+    }];
+    [op1 addDependency:op2];
+    [[NSOperationQueue mainQueue] addOperation:op1];
+    [[NSOperationQueue mainQueue] addOperation:op2];
+}
+- (IBAction)markClick:(id)sender {
+    if (!_isLogin) {
+        NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{            
+        }];
+    }
+}
+
+- (void)mark {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"提示"message:@"拨打客服电话"preferredStyle:UIAlertControllerStyleAlert];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 
